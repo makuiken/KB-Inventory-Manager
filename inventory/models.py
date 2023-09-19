8ef1d32b9da8e89d0efb95b3b387d633ddaa831b
Micah Kuiken <mkuiken18@outlook.com>
2023-09-19 19:59:06 -0400

    Add model for board and length quantity tracking

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Lumber(models.Model):
+    ref_id = models.CharField(unique=True, primary_key=True, max_length=30)
+    name = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+class Length(models.Model):
+    ref_id = models.ForeignKey(Lumber, on_delete=models.CASCADE)
+    length = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.ref_id}: {self.length}' - {self.quantity}"
