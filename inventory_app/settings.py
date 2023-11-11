d88e7c678ae71b1ea95fe899b3809feb157ba8ab
Micah Kuiken <mkuiken18@outlook.com>
2023-11-11 11:47:48 -0500

    Allow railway as host

--- inventory_app/settings.py
+++ inventory_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.com', '127.0.0.1']
 
 
 # Application definition
